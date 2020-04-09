55a53646fc9706024c1f2c0e0d8c484c8024c519
Rinat <37953890+khusainovrm@users.noreply.github.com>
2020-04-09 10:18:19 +0200

    Function displayStore

--- js/index.js
+++ js/index.js
@@ -5,38 +5,38 @@ window.onload = () => {
 
 function initMap() {
     var Rome = {
-        lat: 41.914208, 
-        lng: 12.502852
+        lat: 34.063584, 
+        lng: -118.376354
     };
     map = new google.maps.Map(document.getElementById('map'), {
         center: Rome,
         zoom: 15,
         mapTypeId: 'roadmap',
     });
-    var myMark1 = {
-        lat: 41.914208, 
-        lng: 12.502852
-    };
-    var myMark2 = {
-        lat: 41.911332, 
-        lng: 12.501737
-    };
-    var marker = new google.maps.Marker({
-        position: myMark1,
-        map: map,
-        title: 'Hello World!'
-      });
-      var marker = new google.maps.Marker({
-        position: myMark2,
-        map: map,
-        title: 'Hello World!'
-      });
 }
 
 
 
 function displayStores () {
-    for (var store of stores) {
-        console.log(store);
+    var storeHtml = ""
+    for (var [index, store] of stores.entries()) {
+        storeHtml+=`
+            <div class="store-container">
+                <div class="store-info">
+                    <div class="store-address">
+                        ${name}
+                    </div>
+                    <div class="store-phone-number">
+                        213-977-07051
+                    </div>
+                </div>
+                <div class="store-number-container">
+                    <div class="store-number">
+                        1
+                    </div>
+                </div>
+            </div>
+        `
     }
+    console.log(storeHtml)
 }
